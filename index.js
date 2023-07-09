3146fb9eca591e45ca43a8f70e866e923dcb564b
Nazmul Hasan18 <nazmul182218@gmail.com>
2023-07-09 11:53:27 +0600

    cart api added

--- index.js
+++ index.js
@@ -14,16 +14,17 @@ app.use(express.json());
 const jwtVerify = (req, res, next) => {
    console.log("hit");
    const authorization = req.headers.authorization;
+
    if (!authorization) {
       return res.status(401).send({ error: true, message: "No authorization Token" });
    }
    const token = authorization.split(" ")[1];
-
    jwt.verify(token, process.env.PRIVATE_KEY, (err, decoded) => {
       if (err) {
          return res.status(401).send({ error: true, message: "Unauthorized User" });
       }
       req.email = decoded.user.email;
+
       next();
    });
 };
@@ -47,23 +48,59 @@ async function run() {
 
       const database = client.db("OnlineBazar");
       const productCollection = database.collection("products");
+      const cartCollection = database.collection("carts");
+      const userCollection = database.collection("users");
 
       // jwt token post
       app.post("/jwt", (req, res) => {
          const user = req.body;
          const token = jwt.sign({ user }, process.env.PRIVATE_KEY, { expiresIn: "1h" });
-         res.send(token);
+         res.send({ token });
       });
-
+      // products
       app.get("/products", async (req, res) => {
          const products = await productCollection.find({}).toArray();
          res.send(products);
       });
+
       app.get("/product/:id", async (req, res) => {
          const id = req.params.id;
 
          const product = await productCollection.findOne({ _id: new ObjectId(id) });
          res.send(product);
+      });
+
+      // carts
+
+      app.put("/cart/:id", jwtVerify, async (req, res) => {
+         const product = req.body.product;
+         const id = req.params.id;
+         const email = req.query.email;
+         const exist = await cartCollection.findOne({ _id: id });
+         const options = { upsert: true };
+         if (exist) {
+            const updateQuantity = {
+               $set: {
+                  quantity: exist.quantity + 1,
+               },
+            };
+
+            const result = await cartCollection.updateOne({ _id: id }, updateQuantity, options);
+            console.log("its updating");
+            return res.send(result);
+         } else {
+            product.email = email;
+            product.quantity = 1;
+            const result = await cartCollection.insertOne(product);
+            return res.send(result);
+         }
+      });
+
+      app.get("/carts/:email", jwtVerify, async (req, res) => {
+         const email = req.params.email;
+
+         const carts = await cartCollection.find({ email }).toArray();
+         res.send(carts);
       });
 
       // Send a ping to confirm a successful connection
